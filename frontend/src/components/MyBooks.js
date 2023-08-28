69f40282bb76d4a6530f78c8ecc34a091f41a125
SurajPhulara <https://github.com/SurajPhulara>
2023-08-28 16:12:24 +0530

    updated my books page

--- frontend/src/components/MyBooks.js
+++ frontend/src/components/MyBooks.js
@@ -5,14 +5,10 @@ import BookCard from './BookCard';
 import { useAuth } from '../contexts/AuthContext';
 
 const MyBooks = () => {
-  // Access user data from AuthContext
   const { user } = useAuth();
-  
-  // State to hold user's owned books
   const [myBooks, setMyBooks] = useState([]);
 
   useEffect(() => {
-    // Fetch user's owned books from the server
     async function fetchMyBooks() {
       try {
         const response = await axios.get('https://bookstore-6oe2.onrender.com/api/books/my-books', {
@@ -20,14 +16,12 @@ const MyBooks = () => {
             Authorization: `Bearer ${user.token}`,
           },
         });
-        // Update state with fetched owned books
         setMyBooks(response.data);
       } catch (error) {
         console.error(error);
       }
     }
 
-    // Fetch owned books if user is authenticated
     if (user) {
       fetchMyBooks();
     }
@@ -35,18 +29,18 @@ const MyBooks = () => {
 
   return (
     <Box maxWidth="800px" mx="auto" p={3}>
-      {/* Display heading */}
       <Typography variant="h4" gutterBottom>My Books</Typography>
-      {/* Grid layout for displaying owned books */}
-      <Grid container spacing={2}>
-        {/* Map through owned books and display as BookCard components */}
-        {myBooks.map((book) => (
-          <Grid item xs={12} sm={6} md={4} key={book._id}>
-            {/* Display each owned book using BookCard component */}
-            <BookCard book={book} />
-          </Grid>
-        ))}
-      </Grid>
+      {myBooks.length === 0 ? (
+        <Typography variant="subtitle1">Books you upload will appear here</Typography>
+      ) : (
+        <Grid container spacing={2}>
+          {myBooks.map((book) => (
+            <Grid item xs={12} sm={6} md={4} key={book._id}>
+              <BookCard book={book} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </Box>
   );
 };
